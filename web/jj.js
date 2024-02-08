41b5d3424aa356fe5fb97a390c578f0ea5d117da
heectorr23 <ejemplo1@gmail.com>
2024-02-08 08:03:21 +0100

    v1

--- web/jj.js
+++
@@ -1,9 +0,0 @@
-function sumaredades() {
-    var edad1 = document.getElementById('edad1').value;
-    var edad2 = document.getElementById('edad2').value;
-    var resultado = parseInt(edad1) + parseInt(edad2);
-    document.getElementById('resultado').value = resultado;
-}
-
-
-sumaredades(5+9);
